b9fac70a5a82fda749b12205285db936e74e9c3d
silversky54 <erick.linares@gmail.com>
2017-05-21 21:19:32 -0400

    commit script and data dictionary

---
+++ README.md
@@ -0,0 +1,23 @@
+Getting and cleaning data course Project
+
+This project is part of coursera Getting and clieaning data course. 
+The main objective of this project is to read data from the "Human Activity Recognition  Analysis", reshape the data as necessary to make it tidy
+and write a new data sat that has the averages of the "mean" and "std" features descrived in the original data. 
+
+This Repo includes the following files
+README.txt: Readme text file with a description of the project and files included
+run_analysis.R: R script which reads Test and Training data from a Human Activity Recognition anlysis and experiment, reshapes it and produces a tidy summarized dataset 
+summ_activity_avg.txt: the summary dataset that is the result of running run_analysis.R
+summ_dataset_info.md: code book of the data and variables included in the summ_activity_avg.txt dataset
+
+run_analysis.R follows the following general process:
+It reads the original Human Activiy Datasets for Test and Training which are each split into 3 files: 
+##1. A 561-feature vector with time and frequency domain variables. 
+##2. Its activity label. 
+##3. An identifier of the subject who carried out the experiment.
+
+In the script then combines all 3 datasets and then merges the Test and Training data sets. From this new dataset we select the variables that represent a mean or standard deviation and reshape the dataset to convert the feature columns which combine feature and variable data into a new tidy dataset.
+Then we we calculate the average of the mean and std variables per subject, activity and feature. The result is stored in a new dataset named "summ_activity_avg.txt".
+
+This scrip requires The Human Activity Recognition Dataset which can be downloaded from the following site including its description:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
---
+++ run_analysis.R
@@ -0,0 +1,83 @@
+##Load Libraries used in this script
+
+library(readr)
+library(dplyr)
+library(tidyr)
+
+##read file "features.txt" to get the column names of the features included in file 
+##"/test/X_test.txt" and "/test/X_train.txt"
+
+##Some variable names are duplicated and will cause a warning but since they won't be 
+##used in this project th warning will be ignored.
+features<-read_delim("./project3/UCI HAR Dataset/features.txt", " ", col_names = c("f_id", "feature"), n_max=561)
+colNames<-features$feature
+
+##561 columns of 16 char each.
+colWidths<-rep(16,561)
+
+##1. Merges the training and the test sets to create one data set.
+##Read test and train data from files. Each train/test data sets are split in three files
+##1. A 561-feature vector with time and frequency domain variables. 
+##2. Its activity label. 
+##3. An identifier of the subject who carried out the experiment.
+##First join these three files per test/train group and then merge both data sets.
+
+##Read Test Features
+X_test<-read_fwf("./project3/UCI HAR Dataset/test/X_test.txt", fwf_widths(colWidths, col_names = colNames))
+##Read Test labels
+Y_test<-read_delim("./project3/UCI HAR Dataset/test/Y_test.txt", " ", col_names = c("activity_id"))
+##Read Test Subjects
+subject_test<-read_delim("./project3/UCI HAR Dataset/test/subject_test.txt", " ", col_names = c("subject"))
+##Merge columns for Test Data
+testdata<-bind_cols(subject_test, Y_test, X_test)
+                    
+##Read Train Features
+X_train<-read_fwf("./project3/UCI HAR Dataset/train/X_train.txt", fwf_widths(colWidths, col_names = colNames))
+##Read Test labels
+Y_train<-read_delim("./project3/UCI HAR Dataset/train/Y_train.txt", " ", col_names = c("activity_id"))
+##Read Test Subjects
+subject_train<-read_delim("./project3/UCI HAR Dataset/train/subject_train.txt", " ", col_names = c("subject"))
+##Merge columns for Test Data
+traindata<-bind_cols(subject_train, Y_train, X_train)
+
+activityData<-dplyr::union(mutate(traindata, tgroup="train"), mutate(testdata, tgroup="test"))
+
+##check for duplicate records and add a unique key to each record
+activityData<-unique(activityData) %>% 
+        mutate(recordN=row_number())
+
+##2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+##Keep Columns with mean and STdev
+
+selectactivityData<-select(activityData, recordN,subject, activity_id, tgroup, matches("[Mm]ean[(]|[Ss]td[(]")) %>%
+        gather(featurevariable, value, -c(recordN, subject,activity_id, tgroup), na.rm=T) %>%
+        separate(featurevariable, c("feature", "variable", "axis")) %>%
+        unite(feature, feature, axis, sep="")%>%
+        spread(variable, value)
+
+
+##3. Uses descriptive activity names to name the activities in the data set
+
+##Read activity labels file from "activity_labels.txt"
+
+label_dim<-read_delim("./project3/UCI HAR Dataset/activity_labels.txt"," ", col_names = c("activity_id", "activity_nm"), n_max = 6)
+
+selectactivityData<-left_join(selectactivityData, label_dim, by="activity_id")
+
+
+## 4. ppropriately labels the data set with descriptive variable names.
+selectactivityData<-mutate(selectactivityData, feature=sub("^t","Time",feature))%>%
+        mutate(feature=sub("^f","Freq",feature)) %>%
+        rename(record_number=recordN, group=tgroup, activity_name=activity_nm)
+        
+
+##5. From the data set in step 4, creates a second, independent tidy data set with 
+##the average of each variable for each activity and each subject.
+
+# Generate second dataset with the averages by activity and subject
+averageactivitydata<-select(selectactivityData, subject, activity_name, feature, mean, std) %>%
+        group_by(activity_name, subject, feature) %>%
+        summarize_each(funs(mean))
+
+write.table(averageactivitydata,"./summ_activity_avg.txt", quote=FALSE, sep=" ",row.names=FALSE)
